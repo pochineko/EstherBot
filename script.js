7fc3d01a1adedb2c14210d30864e482a35ac723c
pochineko <dlethanh82@gmail.com>
2016-05-11 17:04:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ module.exports = new Script({
                 }
 
                 if (!_.has(scriptRules, upperText)) {
-                    return bot.say(`I didn't understand that.`).then(() => 'speak');
+                    return bot.say(`I didn't understand that. What do you wanna do?\n%[Show me the activities](postback:activities)`).then(() => 'speak');
                 }
 
                 var response = scriptRules[upperText];
